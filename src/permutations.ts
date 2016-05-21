97b5dcbaea20088cb2828820687e03eb078431de
Lucio Paiva <luciopaiva@gmail.com>
2016-05-21 13:06:47 -0300

    Permutation second optimization: no need to create arrays when starting the last sub-branch of each branch
    For N=4, the initial implementation created 128 arrays. After the first optimization, it went down to 80. Now it creates only 46.

--- src/permutations.ts
+++ src/permutations.ts
@@ -11,14 +11,23 @@ export function permutations(itemsLeft: any[], prefix: any[] = [], permutationsS
             prefix.push(item);
             permutationsSoFar.push(prefix);
         } else {
-            // add that item to the prefix
-            let newPrefix = ArrayUtil.clone(prefix);
-            arraysCreated++;
-            newPrefix.push(item);
+            let newPrefix;
+            let newItemsLeft;
 
-            // and then subtract it from the items left
-            let newItemsLeft = ArrayUtil.clone(itemsLeft);
-            arraysCreated++;
+            if (index == itemsLeft.length - 1) {
+                // if this is the last sub-branch in this branch, reuse existing arrays instead of cloning them
+                newPrefix = prefix;
+                newItemsLeft = itemsLeft;
+            } else {
+                // add that item to the prefix
+                newPrefix = ArrayUtil.clone(prefix);
+                arraysCreated++;
+
+                // and then subtract it from the items left
+                newItemsLeft = ArrayUtil.clone(itemsLeft);
+                arraysCreated++;
+            }
+            newPrefix.push(item);
             newItemsLeft.splice(index, 1);
 
             // if there are items left to be processed, do a recursion
@@ -28,7 +37,7 @@ export function permutations(itemsLeft: any[], prefix: any[] = [], permutationsS
     return permutationsSoFar;
 }
 
-let result = permutations('abc'.split(''));
+let result = permutations('abcd'.split(''));
 result.forEach(perm => console.info(perm));
 console.info('Total permutations: ' + result.length);
 console.info('Arrays created: ' + arraysCreated);
