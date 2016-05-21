4569aaa4a4fa5a12e0c2b09d37b78b2c49394922
Lucio Paiva <luciopaiva@gmail.com>
2016-05-21 12:44:29 -0300

    Permutation first optimization: no need to create arrays at the leaves of the recursion tree

--- src/permutations.ts
+++ src/permutations.ts
@@ -6,20 +6,21 @@ let arraysCreated = 0;
 export function permutations(itemsLeft: any[], prefix: any[] = [], permutationsSoFar: any[] = []): any[][] {
     // for each item left
     itemsLeft.forEach((item, index) => {
-        // add that item to the prefix
-        let newPrefix = ArrayUtil.clone(prefix);
-        arraysCreated++;
-        newPrefix.push(item);
-
-        // and then subtract it from the items left
-        let newItemsLeft = ArrayUtil.clone(itemsLeft);
-        arraysCreated++;
-        newItemsLeft.splice(index, 1);
-
-        if (newItemsLeft.length == 0) {
+        if (itemsLeft.length == 1) {
             // if we have a whole permutation, save it and stop
-            permutationsSoFar.push(newPrefix);
+            prefix.push(item);
+            permutationsSoFar.push(prefix);
         } else {
+            // add that item to the prefix
+            let newPrefix = ArrayUtil.clone(prefix);
+            arraysCreated++;
+            newPrefix.push(item);
+
+            // and then subtract it from the items left
+            let newItemsLeft = ArrayUtil.clone(itemsLeft);
+            arraysCreated++;
+            newItemsLeft.splice(index, 1);
+
             // if there are items left to be processed, do a recursion
             permutations(newItemsLeft, newPrefix, permutationsSoFar);
         }
@@ -27,7 +28,7 @@ export function permutations(itemsLeft: any[], prefix: any[] = [], permutationsS
     return permutationsSoFar;
 }
 
-let result = permutations('abcd'.split(''));
+let result = permutations('abc'.split(''));
 result.forEach(perm => console.info(perm));
 console.info('Total permutations: ' + result.length);
 console.info('Arrays created: ' + arraysCreated);
